b79dcb271337d482551aa985ce9c766e4d32c93d
Toinou3010 <cyrilplanes33@gmail.com>
2021-04-10 18:56:15 +0200

    Ex 8

---
+++ Exo-8.rb
@@ -0,0 +1,5 @@
+puts "C'est quoi ton prénom ?" #affiche la phrase
+print "> " #affiche > mais ne renvoie pas à la ligne suivante
+user_name = gets.chomp #Déclare la Variable user_name et attend une valeur de la part de l'utilisateur
+
+puts "Bonjour, #{user_name} !" #affiche Bonjour, le nom rentré par l'utilisateur
