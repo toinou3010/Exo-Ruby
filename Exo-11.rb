1d7005a562958b04159d8e154c9a9dae6fc268bf
Toinou3010 <cyrilplanes33@gmail.com>
2021-04-10 19:37:05 +0200

    Ex 11

---
+++ Exo-11.rb
@@ -0,0 +1,7 @@
+puts "Donne moi un nombre" #affiche la phrase
+print ">" #affiche > mais ne renvoie pas à la ligne suivante
+number = gets.chomp #Déclare la Variable age et attend une valeur de la part de l'utilisateur
+
+for i in 0..number.to_i
+puts "Salut, ça farte ?"
+end
