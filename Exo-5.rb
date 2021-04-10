b347ac9555c29e38f0cad724af8a2c3a0e6df4f6
Toinou3010 <cyrilplanes33@gmail.com>
2021-04-10 18:18:53 +0200

    Ex 5

---
+++ Exo-5.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #affiche la phrase contenue entre les ""
+puts "Travail : #{10 * 5 * 11}" #affiche "travail" plus le résultat de l'opération contenu dans #{} (en décimale)
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #affiche la phrase plus le résulta de l'opération contenu dans #{} (en décimale) 
+
+puts "Et en secondes ?" #affiche la phrase "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 #affiche le résultat de l'opération (en décimale)
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" #affiche la phrase
+
+puts 3 + 2 < 5 - 7 #affiche le résultat de l'opération (true or false)
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #affiche la phrase plus le résultat de l'opération dans #{} (en décimale)
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"# affiche la phrase plus le résultat de l'opération dans #{} (en décimale)
+
+puts "Ok, c'est faux alors !" #affiche la phrase
+
+puts "C'est drôle ça, faisons-en plus :" #affiche la phrase
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #affiche la phrase plus le résulta de l'opération contenu dans #{} (true or false)
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #affiche la phrase plus le résulta de l'opération contenu dans #{} (true or false)
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"#affiche la phrase plus le résulta de l'opération contenu dans #{}  (true or false)
