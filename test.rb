8e14181b71bc001646c704d4e8127ffaa80f3ef0
Toinou3010 <cyrilplanes33@gmail.com>
2021-04-11 23:07:15 +0200

    a delet

---
+++ test.rb
@@ -0,0 +1,22 @@
+
+print ">"
+
+number = gets.chomp
+
+c = number.to_i
+
+for i in 0..number.to_i
+a = " "
+b = "#"
+
+print a * c
+d = i + i
+e = d.to_i
+
+f = e - 1
+
+c -= 1
+
+puts b * f.to_i
+
+end
